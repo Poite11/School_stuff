44dc6824a023cc55ad5d1d37af951f45ad658416
Kamil Trzciński <63679025+Poite11@users.noreply.github.com>
2026-03-18 23:20:16 +0100

    Refactor school_pythons.py with additional functions

--- school_pythons.py
+++ school_pythons.py
@@ -139,6 +139,97 @@ print(f"{games} games played: {cool_game(game_rules,"a",games)}\n")
 
 
 
+import random
+
+def move_indices(indices, max_val):
+
+
+	ind_len = len(indices)
+	max_range = max_val - ind_len
+
+
+	# finding, from the back, the first position that hasn't reached its max value
+	place_to_change = None
+	for x in range(ind_len-1,-1,-1):
+
+		if indices[x] < max_range + x + 1:
+			place_to_change = x
+			break
+
+
+	if place_to_change == None: return None
+
+
+	# +1 to that position
+	indices[place_to_change] += 1
+	# indices_prime[ind_len-place_to_change] -= 1
+	
+
+
+	# reset everything to the right of the position
+	for x in range(place_to_change+1, len(indices)):
+
+		indices[x] = indices[x-1] + 1
+
+
+	return indices
+
+
+def find_subsets(A,n):
+
+	Subsets_of_size_n = []
+	Indices = list(range(n))
+
+	while Indices is not None:
+		
+		Subsets_of_size_n.append([A[x] for x in Indices])
+		Indices = move_indices(Indices,len(A)-1)
+
+	return Subsets_of_size_n
+
+
+def find_unique_sums(A,n):
+
+	unique_sums = set()
+	Indices = list(range(n))
+	# Indices_prime = list(range(len(A)-1,n-1,-1)) 
+
+	# print(Indices)
+
+	while Indices is not None:
+		
+		unique_sums.add(sum([A[x] for x in Indices]))
+		Indices = move_indices(Indices,len(A)-1)
+		# Indices_prime = [list(range())]
+
+	return unique_sums
+
+
+
+
+
+A = list(range(1,23))
+n = 5
+
+A = [random.randint(1,100) for x in range(1,32)]
+
+A = list(set(A))
+A = [1, 2, 3, 5, 19, 22, 23, 31, 34, 35, 43, 45, 47, 56, 60, 66, 68, 69, 70, 76, 80, 81, 87]
+# A = [1, 2, 3,4,5,6,7,8,9,10]
+
+print(len(A))
+
+for n in range(len(A)+1):
+	# kamil = find_subsets(A,n)
+	# itertools_ = [list(comb) for comb in itertools.combinations(A,n) ]
+	# print(kamil == itertools_,"  ", len(kamil))
+
+	print(len(find_unique_sums(A,n)))
+
+	# print(len(kamil))
+	# sums = [sum(sett) for sett in kamil]
+	# print(len(set(sums)))
+
 
 
 
